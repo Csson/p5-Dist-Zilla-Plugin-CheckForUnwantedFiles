29f43cb8f3c0bb3837c10addbe0e595d34fdca8f
Erik Carlsson <info@code301.com>
2020-12-29 17:16:12 +0000

    Initial development done

--- lib/Dist/Zilla/Plugin/CheckForUnwantedFiles.pm
+++ lib/Dist/Zilla/Plugin/CheckForUnwantedFiles.pm
@@ -4,11 +4,46 @@ use warnings;
 
 package Dist::Zilla::Plugin::CheckForUnwantedFiles;
 
-# ABSTRACT: Short intro
+# ABSTRACT: Check for unwanted files
 # AUTHORITY
 our $VERSION = '0.0100';
 
+use Moose;
+use namespace::autoclean;
+use Path::Tiny;
+use Types::Standard qw/ArrayRef/;
 
+with qw/
+    Dist::Zilla::Role::AfterBuild
+/;
+
+has unwanted_file => (
+    is => 'ro',
+    isa => ArrayRef,
+    default => sub { [] },
+);
+
+sub mvp_multivalue_args { qw/unwanted_file/ }
+
+sub after_build {
+    my $self = shift;
+    my $root_path = path('.');
+
+    my @existing_unwanted_paths = ();
+    for my $unwanted (@{ $self->unwanted_file }) {
+        push @existing_unwanted_paths => $unwanted if $root_path->child($unwanted)->exists;
+    }
+
+    if (scalar @existing_unwanted_paths) {
+        $self->log('The following unwanted files exist:');
+        for my $unwanted (@existing_unwanted_paths) {
+            $self->log("* $unwanted");
+        }
+        $self->log_fatal('Build aborted.');
+    }
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 
@@ -18,12 +53,27 @@ __END__
 
 =head1 SYNOPSIS
 
-    use Dist::Zilla::Plugin::CheckForUnwantedFiles;
+In C<dist.ini> (though it is more useful in a C<PluginBundle>):
+
+    [CheckForUnwantedFiles]
+    unwanted_file = .travis.yml
+    unwanted_file = .github/
 
 =head1 DESCRIPTION
 
-Dist::Zilla::Plugin::CheckForUnwantedFiles is ...
+This plugin checks the development directory (not the build directory) for unwanted files. This is useful when, for instance, switching CI providers, and you don't
+want to have the previous provider's configuration files lingering around B<and> you are too forgetful to remember to check for them
+when doing a new release after the switch.
 
-=head1 SEE ALSO
+It is run at the C<AfterBuild> stage, and takes one (repeatable) argument: C<unwanted_file>. It is a fatal error if any unwanted file is found.
+And, despite its name, it works just as well with unwanted directories.
+
+So:
+
+=for :list
+1. Remove the plugin that generates the file from the bundle
+2. Add this plugin to the bundle
+3. Add the path to the file gets generated as an C<unwanted_file>
+4. You must delete the unwanted file before the distribution can be built
 
 =cut
